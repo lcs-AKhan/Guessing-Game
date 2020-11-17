ecab92f1ee9f83571ee3abc8c5ed6cbb3abf453e
Abdul Khan <akhan@lcs.on.ca>
2020-11-17 15:05:33 -0500

    Completed basic Number guessing game

--- Guessing Game/ContentView.swift
+++ Guessing Game/ContentView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var number = Int.random(in: 1...100)
+    
+    @State var guessString = ""
+    @State var guess = 0
+    
+    @State private var tries = 0
+    
+    @State private var stateOfAnswer = ""
+    
+    @State private var numberEvenOrOdd = ""
+    
+    @State private var hint = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Guessing Game")
+                .font(.largeTitle)
+                .fontWeight(.regular)
+                .multilineTextAlignment(.center)
+                .padding(.bottom)
+            Form {
+                Section {
+                    Text("Im thinking of a number between 1 and 100 ... Can you guess this number?")
+                        .font(.title)
+                        .padding(.horizontal)
+                }
+                Section {
+                    TextField("Enter guess here", text: $guessString)
+                    Button(action: {
+                        CheckGuess()
+                    }) {
+                        Text("Submit Guess")
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                Section {
+                    Text("\(stateOfAnswer)")
+                    Text("Tries: \(tries)")
+                }
+            }
+        }
+    }
+    func CheckGuess() {
+        guess = Int(guessString) ?? 0
+        if guess == number {
+            stateOfAnswer = "You are right!"
+        } else if guess < number {
+            stateOfAnswer = "Guess higher"
+        } else {
+            stateOfAnswer = "Guess lower"
+        }
+        tries += 1
+    }
+    func Hints() {
+        let hintChooser = Int.random(in: 1...10)
+        if hintChooser > 5 {
+            let numberplus10 = number + 10
+            hint = "Number is below \(numberplus10)"
+        }
+        
     }
 }
 
